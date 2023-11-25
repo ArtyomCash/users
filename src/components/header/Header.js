f3c985c3d4d522af793effdb4cbcf0a4d75776c8
Artyom <cash.kin.art@gmail.com>
2023-11-25 14:06:58 +0200

    prettire

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,9 +1,7 @@
 import styles from './header.module.scss';
 
 const Header = () => {
-  return <header className={styles.header}>
-    Header
-  </header>
+  return <header className={styles.header}>Header</header>;
 };
 
 export default Header;

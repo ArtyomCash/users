e4903a150484a1df871395bf23e6f246c5c2c10a
Artyom <cash.kin.art@gmail.com>
2023-11-26 13:40:26 +0200

    search user name

--- src/containers/Main/Main.jsx
+++ src/containers/Main/Main.jsx
@@ -1,27 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import styles from './main.module.scss';
 
 const Main = () => {
-  const [postList, setPostList] = React.useState(null);
+  const [postList, setPostList] = useState(null);
+  const [searchTerm, setSearchTerm] = useState('');
 
-  React.useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/posts')
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
       .then((response) => response.json())
       .then((json) => setPostList(json));
   }, []);
 
-  const uniqueUsers =
-    postList && Array.from(new Set(postList.map((post) => post.userId)));
+  const filteredUsers = postList
+    ? postList.filter((user) =>
+        user.username.toLowerCase().includes(searchTerm.toLowerCase())
+      )
+    : [];
 
   return (
     <main className={styles.main}>
       <h1>Users list</h1>
+      <div>
+        <input
+          type='text'
+          placeholder='Search by username'
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+      </div>
       <section className={styles.wrapper}>
-        {uniqueUsers &&
-          uniqueUsers.map((userId, index) => (
-            <Link to={`/user-posts/${userId}`} key={index}>
-              User name {userId}
+        {filteredUsers &&
+          filteredUsers.map((user) => (
+            <Link to={`/user-posts/${user.id}`} key={user.id}>
+              User name {user.username}
             </Link>
           ))}
       </section>
